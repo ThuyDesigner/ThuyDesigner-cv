021dd518aa0caca375aa109226236e23b7336d16
ThuyDesigner <ngthithuthuy96@gmail.com>
2022-02-05 16:19:53 +0700

    update reponsive

--- src/component/TimeLine/TimeLine.js
+++ src/component/TimeLine/TimeLine.js
@@ -3,23 +3,22 @@ import classNames from "classnames";
 import "./timeline.scss";
 
 const TimeLine = ({ times = [], content = "", addClass = "", children }) => {
-  return (
-    <div className={classNames("gr-timeline pt-3", addClass)}>
-      <div className="row d-flex align-items-start">
-        <div className="time col-lg-3 col-md-3 col-3  pl-2">
-        <ul>{times.map(time =>
+return (
+<div className={classNames("gr-timeline pt-3", addClass)}>
+  <div className="row d-flex align-items-start">
+    <div className="time col-lg-3 col-md-3 col-3  pl-2">
+      <ul>{times.map(time =>
         <li> <span className="text-small">{time.small}</span>{time.time} </li>
-          )}
-          
-        </ul>
-        </div>
-        <div className=" col-lg-9 col-md-9 col-9 text-break">
-          <div className="content">{content} </div>
-          <div> {children} </div>
-        </div>
-      </div>
+        )}
+      </ul>
     </div>
-  );
+    <div className=" col-lg-9 col-md-9 col-9 text-break">
+      <div className="content">{content} </div>
+      <div> {children} </div>
+    </div>
+  </div>
+</div>
+);
 };
 
 export default TimeLine;
